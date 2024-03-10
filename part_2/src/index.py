523eb59ed07921005630bbc896b996029094c0f4
ngoakor12 <ngoakor12@gmail.com>
2024-03-10 20:37:55 +0200

    program can read from a file path

--- part_2/src/index.py
+++ part_2/src/index.py
@@ -1,6 +1,13 @@
 import re
 import os
 
+SERIES_LIST = [
+    ["Dragonball", "original"],
+    ["Dragonball Z", "z"],
+    ["Dragonball Z Kai", "z_kai"],
+    ["Dragonball GT", "gt"],
+    ["Dragonball Super", "super"],
+]
 SUPPORTED_FILE_EXTENSIONS = ["mkv", "mp4"]
 INVALID_EPISODE_NUMBER_ERROR = "File should have an episode number"
 INVALID_EXTENSION_ERROR = "File should have an extension"
@@ -9,7 +16,13 @@ UNSUPPORTED_FILE_EXTENSIONS_ERROR = f"Unsupported file extension. Only the follo
 
 def main(directory, series=None):
     cleaned_file_names = []
-    for file in directory:
+    if not series:
+        series = get_series_code_from_directory(directory)
+    series_folder = os.listdir(directory)[0].split("/")[-1]
+    # print(directory)
+    # print(series_folder)
+    for file in os.listdir(f"{directory}/{series_folder}"):
+        print(file)
         cleaned_file_names.append(get_cleaned_file_name(file, series))
 
     return cleaned_file_names
@@ -21,12 +34,10 @@ def get_cleaned_file_name(file, series=None):
     episode_number = get_episode_number(file)
     extension = get_file_extension(file)
 
-    if series:
+    if series == "original":
+        file_name = f"db-{episode_number}.{extension}"
+    else:
         file_name = f"db-{series}-{episode_number}.{extension}"
-
-        return file_name
-
-    file_name = f"db-{episode_number}.{extension}"
 
     return file_name
 
@@ -38,7 +49,7 @@ def get_episode_number(file):
         padded_number_str = str(number).zfill(3)
 
         return padded_number_str
-
+    print("**", file)
     raise ValueError(INVALID_EPISODE_NUMBER_ERROR)
 
 
@@ -56,6 +67,13 @@ def get_file_extension(file=None):
     raise ValueError(INVALID_EXTENSION_ERROR)
 
 
+def get_series_code_from_directory(directory):
+    series_folder = os.listdir(directory)[0].split("/")[-1]
+    for name, code in SERIES_LIST:
+        if name.lower() == series_folder.lower():
+            return code
+
+
 if __name__ == "__main__":
-    directory = os.listdir("data")
-    print(main(directory, "z"))
+    directory = f"data"
+    print(main(directory))
