9ef861c1af7bc934234c5bb4ea340d5ef2c0944d
ngoakor12 <ngoakor12@gmail.com>
2024-03-11 03:35:40 +0200

    add part 5

--- part_5/src/index.py
+++ part_5/src/index.py
@@ -1,6 +1,8 @@
 import re
 import os
-from colorama import Fore, Back, Style
+import sys
+import shutil
+from colorama import Fore, Style
 
 SERIES_LIST = [
     ["Dragonball", "original"],
@@ -15,7 +17,20 @@ INVALID_EXTENSION_ERROR = "File should have an extension"
 UNSUPPORTED_FILE_EXTENSIONS_ERROR = f"Unsupported file extension. Only the following file extensions are supported: {', '.join(SUPPORTED_FILE_EXTENSIONS)}"
 
 
+env_vars = sys.argv[1:]
+source_path = env_vars[0]
+target_path = env_vars[1] if os.path.isdir(env_vars[1]) else os.mkdir(env_vars[1])
+
+
 def main(directory, series=None):
+
+    # create sub directories
+    for series in SERIES_LIST:
+        if os.path.exists(os.path.join(target_path).lower()):
+            os.mkdir(os.path.join(target_path, series[0].lower()))
+
+    if not (source_path and target_path):
+        raise Exception("source and or target paths missing!")
 
     for current_dir, dirs, files in os.walk(directory):
         series_folder = current_dir.split("/")[-1]
@@ -26,26 +41,24 @@ def main(directory, series=None):
                 current_file_path = os.path.join(current_dir, current_file)
 
                 new_file = get_cleaned_file_name(current_file, series)
-                new_file_path = os.path.join(current_dir, new_file)
+                new_file_path = os.path.join(
+                    target_path, current_dir.split("/")[-1].lower(), new_file
+                )
 
-                if current_file != new_file:
-                    os.rename(
-                        current_file_path,
-                        new_file_path,
-                    )
+                shutil.copy2(
+                    current_file_path,
+                    new_file_path,
+                )
 
-                    print(
-                        current_file,
-                        "->",
-                        new_file,
-                        "|",
-                        Fore.GREEN,
-                        f"success",
-                        Style.RESET_ALL,
-                    )
-
-                else:
-                    print(new_file, "|", Fore.YELLOW, "skipped", Style.RESET_ALL)
+                print(
+                    current_file,
+                    "->",
+                    new_file,
+                    "|",
+                    Fore.GREEN,
+                    f"success",
+                    Style.RESET_ALL,
+                )
 
 
 def get_cleaned_file_name(file, series="original"):
@@ -94,5 +107,5 @@ def get_series_code_from_directory(directory):
 
 
 if __name__ == "__main__":
-    root_directory = f"data"
+    root_directory = source_path
     main(root_directory)
